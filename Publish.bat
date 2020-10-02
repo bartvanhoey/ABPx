031eac7139928b66cbb521b9414be4463a0e53a7
bartvanhoey <bartvanhoey@hotmail.com>
2020-10-02 20:14:56 +0200

    v0.0.2

---
+++ Publish.bat
@@ -0,0 +1,28 @@
+if "%~1"=="" SET argument=patch
+if "%~1"=="patch" SET result=true
+if "%~1"=="minor" SET result=true
+if "%~1"=="major" SET result=true
+if "%result%" == "true" (
+    SET argument=%1
+)
+
+FOR /f %%i in ('node -pe "require('./package.json')['version']"') do @SET VersionNumber="%%i"
+
+FOR /F "tokens=1 delims=." %%A IN (%VersionNumber%) DO SET MajorVersionNumber=%%A
+FOR /F "tokens=2 delims=." %%A IN (%VersionNumber%) DO SET MinorVersionNumber=%%A
+FOR /F "tokens=3 delims=." %%A IN (%VersionNumber%) DO SET PatchVersionNumber=%%A
+
+if %argument%==major (SET /a MajorVersionNumber=MajorVersionNumber+1)
+if %argument%==minor (SET /a MinorVersionNumber=MinorVersionNumber+1)
+if %argument%==patch (SET /a PatchVersionNumber=PatchVersionNumber+1)
+
+SET CommitMessage=v%MajorVersionNumber%.%MinorVersionNumber%.%PatchVersionNumber%
+
+echo %CommitMessage%
+
+git add .
+git commit -m %CommitMessage%
+git push
+
+vsce publish patch
+
